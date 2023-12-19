42974fbd5a6c0545fd386efae9ed7e228557ccc4
michael mayobre <mbmayobre@gmail.com>
2023-12-18 21:48:56 -0500

    removed dead code from app, created separate buttons component for download buttons

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,7 @@ import "./styles/tailwind.scss";
 import Home from "./views/Home";
 
 function App() {
-  return (
-    <Home />
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+  return <Home />;
 }
 
 export default App;
